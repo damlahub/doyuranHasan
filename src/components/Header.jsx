b569c0a758ba940a097b10adbec66bafa38f999c
damla <damlaoksel@hotmail.com>
2026-07-28 20:07:54 +0300

    Header.jsx created

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>DOYURAN HASAN</h1>
+            <span className='underline'></span>
+            <p>Doymanın en lezzetli hali.</p>
+        </header>
+    )
+}
+
+export default Header;
